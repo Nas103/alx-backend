4e6ba0d4a352b5a1d074836926dd3f6a5f720df1
Nas103 <tumelorhulani33@gmail.com>
2024-10-29 03:17:37 +0000

    alx-backend

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module for pagination helper function.
+"""
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Calculate the start and end indexes for pagination.
+
+    Args:
+        page (int): The current page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple: A tuple containing the start index and the end index.
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
