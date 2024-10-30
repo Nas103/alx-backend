7678baed1c8387463d4a5b4dbdfab1f751f2c3fb
Nas103 <tumelorhulani33@gmail.com>
2024-10-30 20:47:16 +0000

    updt

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,60 +1,34 @@
 #!/usr/bin/python3
-""" MRUCache module
-"""
-
 from base_caching import BaseCaching
 
 
 class MRUCache(BaseCaching):
-    """ MRUCache defines a caching system with MRU eviction policy """
-
     def __init__(self):
-        """ Initialize the cache """
         super().__init__()
-        self.mru_key = None
+        self.usage_order = []
 
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key is not None and item is not None:
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
             self.cache_data[key] = item
-            self.mru_key = key
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                discarded_key = self.mru_key
-                del self.cache_data[discarded_key]
-                print(f"DISCARD: {discarded_key}")
+            self.usage_order.remove(key)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                self._evict()
+            self.cache_data[key] = item
+
+        self.usage_order.append(key)
 
     def get(self, key):
-        """ Get an item by key """
-        if key in self.cache_data:
-            self.mru_key = key
-            return self.cache_data[key]
-        return None
+        if key is None or key not in self.cache_data:
+            return None
+        self.usage_order.remove(key)
+        self.usage_order.append(key)
+        return self.cache_data[key]
 
-
-if __name__ == "__main__":
-    my_cache = MRUCache()
-    my_cache.put("A", "Hello")
-    my_cache.put("B", "World")
-    my_cache.put("C", "Holberton")
-    my_cache.put("D", "School")
-    my_cache.print_cache()
-    print(my_cache.get("B"))  # Output: World
-    my_cache.put("E", "Battery")
-    my_cache.print_cache()
-    my_cache.put("C", "Street")
-    my_cache.print_cache()
-    print(my_cache.get("A"))  # Output: Hello
-    print(my_cache.get("B"))  # Output: None
-    print(my_cache.get("C"))  # Output: Street
-    my_cache.put("F", "Mission")
-    my_cache.print_cache()
-    my_cache.put("G", "San Francisco")
-    my_cache.print_cache()
-    my_cache.put("H", "H")
-    my_cache.print_cache()
-    my_cache.put("I", "I")
-    my_cache.print_cache()
-    my_cache.put("J", "J")
-    my_cache.print_cache()
-    my_cache.put("K", "K")
-    my_cache.print_cache()
+    def _evict(self):
+        mru_key = self.usage_order.pop()
+        print(f"DISCARD: {mru_key}")
+        del self.cache_data[mru_key]
